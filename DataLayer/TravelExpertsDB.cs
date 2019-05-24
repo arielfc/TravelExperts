4f3dd3c5d01de28a8be644d50f59a8e73ee57a36
Barbara <barbarayyang@hotmail.com>
2019-05-24 14:51:17 -0600

    Revert "Remote Database Added"
    This reverts commit 3510071ae2e8adcb48ce1fc063fa8a8a3b65b820.

--- DataLayer/TravelExpertsDB.cs
+++ DataLayer/TravelExpertsDB.cs
@@ -11,37 +11,11 @@ namespace DataLayer
     {
         public static SqlConnection GetConnection()
         {
-            // Establish Connection to Local DB
-            /*
+            // Establish Connection to DB
             SqlConnection connection = new SqlConnection();
-            string ConnectionString = "Data Source=DESKTOP-KCKTTB9\\SQLEXPRESS;" +
-                "Initial Catalog=TravelExperts;Integrated Security=true";
+            string ConnectionString = "Data Source=DESKTOP-KCKTTB9\\SQLEXPRESS;Initial Catalog=TravelExperts;Integrated Security=true";
             connection.ConnectionString = ConnectionString;
             connection.Open();
-            return connection;
-            */
-            
-            // Remote DB 20190524 
-            
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "softdev0523.database.windows.net";
-            builder.UserID = "softdevlogin";
-            builder.Password = "softDevma3$";
-            builder.InitialCatalog = "TravelExperts";
-            SqlConnection connection = new SqlConnection(builder.ConnectionString);
-            
-            try
-            {
-                connection.Open();
-            }
-            catch (Exception ex)
-            {
-                ;
-            }
-            finally
-            {
-                ;
-            }
             return connection;
         }
     }
