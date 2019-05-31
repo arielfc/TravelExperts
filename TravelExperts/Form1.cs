ebfd09159a87a771bc0ce77d12763d2cdd2ce356
Williamyan0715 <weiguang.yan@hotmail.com>
2019-05-31 11:49:17 -0600

    Add penel13 in Form1 for product update &add

--- TravelExperts/Form1.cs
+++ TravelExperts/Form1.cs
@@ -24,13 +24,18 @@ namespace TravelExperts
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.panel14.SendToBack();
-            this.panel4.BringToFront();
+            this.panel14.BringToFront();
+            this.panel14.BringToFront();
+            this.panel13.BringToFront();
             //dataGridView1.DataSource = DataLayer.PackagesDB.GetPackages();
 
             comboBox1.DataSource = packageManager.GetPackage();
             comboBox1.DisplayMember = "PkgName";
             comboBox1.ValueMember = "PkgName";
+
+            // Invisible Panel 13 for products information
+            panel13.Visible = false;
+
         }
         // Allows Dragging Borderless Form
         protected override void WndProc(ref Message m)
@@ -65,9 +70,19 @@ namespace TravelExperts
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            this.panel4.SendToBack();
-            this.panel14.BringToFront();
-           
+            this.panel14.SendToBack();
+            //this.panel14.Visible = false;
+            this.panel15.SendToBack();
+            //this.panel15.Visible = false;
+            //Show panel13
+            this.panel13.BringToFront();
+            this.panel13.Visible = true;
+
+            comboBox2.DataSource = ProductsDB.GetProducts();
+            comboBox2.DisplayMember = "ProdName";
+            comboBox2.ValueMember = "ProdName";
+
+
             RadioButton rb = sender as RadioButton;
             if (rb != null)
             {
@@ -161,8 +176,9 @@ namespace TravelExperts
 
         private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
-            this.panel14.SendToBack();
-            this.panel4.BringToFront();
+            this.panel13.SendToBack();
+            this.panel14.BringToFront();
+            this.panel15.BringToFront();
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -186,6 +202,108 @@ namespace TravelExperts
         private void panel15_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == -1)
+                return;
+
+            Product pdt = new Product();
+            pdt = ProductsDB.GetProductByID(comboBox2.SelectedIndex + 1);
+
+            txtUptProduct.Text = pdt.ProdName;
+        }
+
+        private void btnUptPdt_Click(object sender, EventArgs e)
+        {
+            string message;
+            string title = "Product Update";
+            MessageBoxButtons button = MessageBoxButtons.OK;
+            MessageBoxIcon icon = MessageBoxIcon.Warning;
+
+            int cunt = comboBox2.Items.Count;
+            for (int i = 0; i < cunt; i++)
+            {
+                if (comboBox2.GetItemText(comboBox2.Items[i]) == txtUptProduct.Text)
+                {
+                    if (comboBox2.SelectedIndex == i)
+                    {
+                        message = "Warning! Product name is not changed.\nPlease input new product name.";
+                    }
+                    else
+                    {
+                        message = "Warning! The updated product is duplicated in list.\nPlease input new product name.";
+                    }
+                    MessageBox.Show(message, title, button, icon);
+                    return;
+                }
+            }
+            int id = ProductsDB.GetProductId(comboBox2.Text);
+            bool flag = ProductsDB.UpdateProduct(id, txtUptProduct.Text);
+            if (flag == true)
+            {
+                int index = comboBox2.SelectedIndex;
+                comboBox2.DataSource = ProductsDB.GetProducts();
+                comboBox2.DisplayMember = "ProdName";
+                comboBox2.ValueMember = "ProdName";
+                comboBox2.SelectedIndex = index;
+                message = "The product is updated successfully!";
+                icon = MessageBoxIcon.None;
+            }
+            else
+            {
+                message = "Error: Fail to update product!\n Please try again or contact IT support.";
+                icon = MessageBoxIcon.Error;
+            }
+            MessageBox.Show(message, title, button, icon);
+        }
+
+        private void btnAddPdt_Click(object sender, EventArgs e)
+        {
+            string message;
+            string title = "New Product Insert";
+            MessageBoxButtons button = MessageBoxButtons.OK;
+            MessageBoxIcon icon;
+            int cunt = comboBox2.Items.Count;
+            for (int i = 0; i < cunt; i++)
+            {
+                if (comboBox2.GetItemText(comboBox2.Items[i]) == txtAddProduct.Text)
+                {
+                    message = "Warning! The product's name has been in list.\nPlease input new product name.";
+                    icon = MessageBoxIcon.Warning;
+                    MessageBox.Show(message, title, button, icon);
+                    return;
+                }
+            }
+            bool flag = ProductsDB.AddProduct(txtAddProduct.Text);
+            if (flag == true)
+            {
+                comboBox2.DataSource = ProductsDB.GetProducts();
+                comboBox2.DisplayMember = "ProdName";
+                comboBox2.ValueMember = "ProdName";
+                comboBox2.SelectedIndex = comboBox2.Items.Count - 1;
+                txtAddProduct.Text = "";
+                message = "The product is added successfully!";
+                icon = MessageBoxIcon.None;
+            }
+            else
+            {
+                message = "Error: Fail to add new product!\n Please try again or contact IT supports.";
+                icon = MessageBoxIcon.Error;
+            }
+            MessageBox.Show(message, title, button, icon);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtUptProduct.Text = comboBox2.Text;
+            txtAddProduct.Text = "";
         }
     }
 }
